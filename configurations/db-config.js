e2520bf9c2b5dd1a220e89a741602a219c1ec920
Aditya Cahyo <aditydcp@gmail.com>
2024-02-10 02:22:26 +0700

    add mysql connection

---
+++ configurations/db-config.js
@@ -0,0 +1,11 @@
+const dbConfig = {
+    db: {
+        host: "127.0.0.1",
+        user: "root",
+        password: "",
+        database: "bookings_csm",
+        connectTimeout: 60000
+    },
+    listPerPage: 10,
+};
+module.exports = dbConfig;
