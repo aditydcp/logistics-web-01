0cf1d6c6450ff90972ed4fc34e91cb2b2d2b30fd
Aditya Cahyo <aditydcp@gmail.com>
2024-02-10 13:23:44 +0700

    add routes to app

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const applicationConfig = require("./configurations/app-config");
 const app = express();
 const port = applicationConfig.port;
 const flightsRouter = require("./routes/flights");
+const bookingsRouter = require("./routes/bookings");
+const airlinesRouter = require("./routes/airlines");
+const airportsRouter = require("./routes/airports");
+const exportersRouter = require("./routes/exporters");
+const importersRouter = require("./routes/importers");
+const usersRouter = require("./routes/users");
 app.use(express.json());
 app.use(
     express.urlencoded({
@@ -14,7 +20,14 @@ app.get("/", (req, res) => {
     res.json({ message: "ok" });
 });
 
+// Configure routes
 app.use("/flights", flightsRouter);
+app.use("/bookings", bookingsRouter);
+app.use("/airlines", airlinesRouter);
+app.use("/airports", airportsRouter);
+app.use("/exporters", exportersRouter);
+app.use("/importers", importersRouter);
+app.use("/users", usersRouter);
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
